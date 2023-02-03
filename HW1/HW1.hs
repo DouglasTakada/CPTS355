188e8a0c1c2a0f5cfc2088461caf2fa6dcb40dce
DouglasTakada <douglas.takada@gmail.com>
2023-02-02 17:50:43 -0800

    Time for Hw #1!!!

---
+++ HW1/HW1.hs
@@ -0,0 +1,40 @@
+-- CptS 355 - Spring 2023 -- Homework1 - Haskell
+-- Name:
+-- Collaborators: 
+
+module HW1
+     where
+
+-- P1(a) count ;  6%
+
+
+-- P1(b) diff ;  6%
+
+
+-- P1(c) bag_diff ; 8%
+
+
+-- P2  everyN ; 10%
+
+
+-- P3(a) make_sparse ; 15%
+
+
+-- P3(b) compress ; 15%
+
+
+-- P4 added_sums ; 8%
+
+
+-- P5 find_routes ; 8%
+
+
+-- P6 group_sum ; 15% 
+
+
+
+-- Assignment rules ; 3%
+-- Your own tests; please add your tests to the HW1Tests.hs file ; 6%
+
+
+
