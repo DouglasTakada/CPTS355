bdf6094e2e47b0f03e5f9dec902709bab46910b2
DouglasTakada <douglas.takada@gmail.com>
2023-02-14 21:28:06 -0800

    Don't understand!

--- Lab 2/Lab2.hs
+++ Lab 2/Lab2.hs
@@ -9,6 +9,23 @@ module Lab2
 -- 1
 {- (a) merge2 -}
 
+merge2 :: [a] -> [a] -> [a]
+merge2 [] [] = []
+merge2 x [] = x
+merge2 [] x = x
+merge2 x y = merge2_helper x y 0
+     where
+          merge2_helper x [] _ = x
+          merge2_helper [] y _ = y
+          merge2_helper [x] (y:ys) counter | (counter == 0) = x : y : ys
+                                           | otherwise = y : merge2_helper x ys (counter - 1)
+          merge2_helper (x:xs) [y] counter | (counter == 0) = x : merge2_helper xs y (counter + 1)
+                                           | otherwise = y : x : xs
+          merge2_helper (x:xs) (y:ys) counter | (counter == 0) = x : merge2_helper xs (y:ys) (counter + 1)
+                                              | otherwise = y : merge2_helper (x:xs) ys (counter - 1)
+
+
+
                          
 
 {- (b) merge2Tail -}
@@ -39,8 +56,8 @@ module Lab2
 
 
 {- (b) concat2Either -}               
-data AnEither  = AString String | AnInt Int
-                deriving (Show, Read, Eq)
+-- data AnEither  = AString String | AnInt Int
+--                 deriving (Show, Read, Eq)
 
 
 
@@ -51,17 +68,17 @@ data AnEither  = AString String | AnInt Int
 
 
 
-data Op = Add | Sub | Mul | Pow
-          deriving (Show, Read, Eq)
+-- data Op = Add | Sub | Mul | Pow
+--           deriving (Show, Read, Eq)
 
-evaluate:: Op -> Int -> Int -> Int
-evaluate Add x y =  x+y
-evaluate Sub   x y =  x-y
-evaluate Mul x y =  x*y
-evaluate Pow x y = x^y
+-- evaluate:: Op -> Int -> Int -> Int
+-- evaluate Add x y =  x+y
+-- evaluate Sub   x y =  x-y
+-- evaluate Mul x y =  x*y
+-- evaluate Pow x y = x^y
 
-data ExprTree a = ELEAF a | ENODE Op (ExprTree a) (ExprTree a)
-                  deriving (Show, Read, Eq)
+-- data ExprTree a = ELEAF a | ENODE Op (ExprTree a) (ExprTree a)
+--                   deriving (Show, Read, Eq)
 
 -- 5 
 {- evaluateTree -}
@@ -75,8 +92,8 @@ data ExprTree a = ELEAF a | ENODE Op (ExprTree a) (ExprTree a)
 
 --7
 {- createRTree -}
-data ResultTree a  = RLEAF a | RNODE a (ResultTree a) (ResultTree a)
-                     deriving (Show, Read, Eq)
+-- data ResultTree a  = RLEAF a | RNODE a (ResultTree a) (ResultTree a)
+--                      deriving (Show, Read, Eq)
 
 
 
