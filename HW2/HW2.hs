339d97bc483d9916281a441e1acede6fbcf262f7
DouglasTakada <douglas.takada@gmail.com>
2023-02-16 22:49:05 -0800

    HW2

---
+++ HW2/HW2.hs
@@ -0,0 +1,45 @@
+-- CptS 355 - Spring 2023 -- Homework2 - Haskell
+-- Name:
+-- Collaborators: 
+
+module HW2
+     where
+
+{- P1 - remove_every, remove_every_tail  -}
+
+-- (a) remove_every – 7%
+
+-- (b) remove_every_tail –  10%
+
+------------------------------------------------------
+{- P2  get_outof_range and count_outof_range  -}
+
+-- (a) get_outof_range – 6%
+
+-- (b) count_outof_range – 10%
+
+------------------------------------------------------
+{- P3  find_routes - 10% -}
+
+------------------------------------------------------
+{- P4  add_lengths and add_nested_lengths -}
+
+-- (a) add_lengths - 6%
+
+-- (b) add_nested_lengths - 10%
+
+------------------------------------------------------
+{- P5 sum_tree and create_sumtree -}
+
+-- (a) sum_tree - 8%
+
+-- (b) create_sumtree - 10%
+
+------------------------------------------------------
+{- P6 list_tree - 16% -}
+
+
+-- Tree examples - 4%
+-- INCLUDE YOUR TREE EXAMPLES HERE
+
+-- Assignment rules 3%
