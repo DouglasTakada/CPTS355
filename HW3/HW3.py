efb63405d489a9f52eb375090b29beea4bc0afe8
DouglasTakada <douglas.takada@gmail.com>
2023-03-09 23:18:32 -0800

    HW3 upload

---
+++ HW3/HW3.py
@@ -0,0 +1,35 @@
+# CptS 355 - Spring 2023 - Assignment 3 - Python
+
+# Please include your name and the names of the students with whom you discussed any of the problems in this homework
+# Name:
+# Collaborators: 
+
+debugging = False
+def debug(*s): 
+     if debugging: 
+          print(*s)
+
+from functools import reduce
+
+## problem 1(a) - aggregate_log  - 5%
+
+
+## problem 1(b) - combine_dict– 6%
+
+
+## problem 1(c) - merge_logs– 12%
+
+
+## problem 2(a) - most_hours – 15%
+
+
+## problem 2(b) - filter_log – 15%
+
+
+## problem 3 - graph_cycle – 12% 
+
+
+## problem 4 - filter_iter – 15% 
+
+
+## problem 5 - merge – 10% 
